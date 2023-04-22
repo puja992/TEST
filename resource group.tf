23fed74d8a5a136e7d22a19336ddcaf4304329f0
Puja Datta <pujadatta992@gmail.com>
2023-04-22 20:16:23 +0530

    RG NEEDS TO BE COMMITTED

---
+++ resource group.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">=0.12"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
+variable "resource_group_location" {
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name_prefix" {
+  default     = "rg"
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
